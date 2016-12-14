61825e173e58696b25e7b35f36e033aed45a4818
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2016-12-14 17:54:39 +0000

    Apply fixes from StyleCI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Assely\Support\Facades\Hook;
 use Assely\Support\Support;
+use Assely\Support\Facades\Hook;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
