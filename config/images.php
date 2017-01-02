b2303f089a724c6e6dae77c069d879be8629ce71
Sören Wrede <soerenwrede@gmail.com>
2017-01-02 19:39:08 +0100

    Add 'medium_large' to image size comment (#3)
    * Add 'medium_large' to image size comment
    
    * Fixes filename in image size config

--- config/images.php
+++ config/images.php
@@ -9,8 +9,8 @@ return [
     |
     | This value determines which thumbnail size will be served by default.
     | WordPress comes with five predefined sizes ('thumbnail', 'medium',
-    | 'large', 'full'). If you want to serve own custom size, create
-    | it inside `Support/sidebars.php` and enter here its name.
+    | 'medium_large', 'large', 'full'). If you want to serve own custom size,
+    | create it inside `Support/thumbnails.php` and enter here its name.
     |
      */
 
