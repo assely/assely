c54dbe4f86de482663c932f9d5627e891a148d6d
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2017-01-02 19:37:50 +0100

    Fixes filename in image size config

--- config/images.php
+++ config/images.php
@@ -10,7 +10,7 @@ return [
     | This value determines which thumbnail size will be served by default.
     | WordPress comes with five predefined sizes ('thumbnail', 'medium',
     | 'medium_large', 'large', 'full'). If you want to serve own custom size,
-    | create it inside `Support/sidebars.php` and enter here its name.
+    | create it inside `Support/thumbnails.php` and enter here its name.
     |
      */
 
