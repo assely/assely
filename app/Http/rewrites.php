70aee8a770a3b0674f77d003c57ceef0f4d1653e
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-01-22 18:24:18 +0100

    Use new Support facade to register theme options

---
+++ app/Http/rewrites.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Custom Rewrite Rules
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the custom rewrite rules,
+| which afterward can be targeted with your routes. Remember
+| to flush rerites after creation or modification rules.
+|
+ */
+
+Rewrite::rule('/{name}')->where([
+	'name' => '([^/]+)'
+]);
