9802e34b806195ac611ed888d6ff9b94f8c8d4f6
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-02-01 22:45:59 +0100

    Setup Laravel Mix public path by default; Add app.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,9 +6,14 @@ const { mix } = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Assely application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.sass('resources/assets/sass/app.scss', 'public/css');
+// Tell Laravel Mix to use the `public` directory.
+mix.setPublicPath('public');
+
+mix // Let's mix our application assets.
+	.sass('resources/assets/sass/app.scss', 'public/css')
+	.js('resources/assets/js/app.js', 'public/js');
