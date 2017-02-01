cea97961155a1aa323e5245bbffff17e16d530fd
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-02-01 22:16:19 +0100

    Mix only styles by default; Fixes tabulators;

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,4 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix
-	.sass('resources/assets/sass/app.scss', 'public/css')
-	.js('resources/assets/js/app.js', 'public/js')
+mix.sass('resources/assets/sass/app.scss', 'public/css');
