0a331ac99b2ef2d8b82a929e5ce7df3d7dc547f9
Jędrzej Chałubek <jedrzej.chalubek@gmail.com>
2017-02-08 22:19:56 +0100

    Changes according to new Rewrite component (#12)

---
+++ app/Http/rewrites.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Custom Rewrite Rules
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the custom rewrite rules,
+| which afterward can be targeted with your routes. Remember
+| to flush rewites after rules creation or modification.
+|
+ */
+
+// Rewrite::rule('/custom/rewrite/{rule}')->where([
+//     'rule' => '([^/]+)',
+// ]);
+
+// Rewrite::endpoint('json', EP_PERMALINK);
